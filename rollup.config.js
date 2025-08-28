c155aa5c6d02bf9719bfbda2fcc9c0f86ca855c2
skskya <dogeliu@126.com>
2025-08-29 01:52:10 +0800

    update comments

--- rollup.config.js
+++ rollup.config.js
@@ -8,15 +8,15 @@ export default [
   {
     input: "src/index.ts",
     output: {
-      name: "shiki",
-      file: "./build/shiki.umd.js",
-      format: "umd",
+      name: "shiki", // 在 <script> 里直接引入，会暴露成全局变量 window.shiki
+      file: "./build/shiki.umd.js", // Rollup 把所有模块打包成一个文件 shiki.umd.js
+      format: "umd", // 打包成通用模块（可以在 Node.js / AMD / 浏览器里直接用）。
     },
     plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-      typescript(), // so Rollup can convert TypeScript to JavaScript
-      terser(), // so Rollup can minify js code
+      resolve(), // 让 Rollup 能解析 node_modules 里的包。
+      commonjs(), // 把 CommonJS 模块 转换成 Rollup 能理解的 ES Module。
+      typescript(), //  Rollup 把 TypeScript 编译成 JavaScript。
+      terser(), // 压缩代码
     ],
   },
 ];
